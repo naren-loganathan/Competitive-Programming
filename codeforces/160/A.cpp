f7dd66cfcf314a37b5f25ba839221622eeef7cb4
Naren Loganathan <naren.spirion@gmail.com>
2020-09-06 11:21:00 +0530

    Add solution for problem `A - Twins` Link: https://codeforces.com/contest/160/problem/A Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/160/submission/91986395

--- codeforces/160/A.cpp
+++ codeforces/160/A.cpp
@@ -1,29 +1,26 @@
-    #include<iostream>
-    #include<algorithm>
-     
-    using namespace std;
-     
-    int main(){
-        int n;
-        cin>>n;
-        int arr[n], sum =0, greedy[n];
-        for(int i=0; i<n;i++){
-            cin>>arr[i];
-            sum+=arr[i];
-            greedy[i]=0;
-        }
-        sort(arr, arr+n);
-        for(int i=n-1;i>0;i--){
-           greedy[i]+=arr[i];
-           greedy[i-1]+=greedy[i];
-        }
-        greedy[0]+=arr[0];
-        for(int i=n-1; i>=0;i--){
-            int b=n-i;
-            if(greedy[i]>(sum-greedy[i])){
-                cout<<b<<endl;
-                break;
-            }
-        }
-        return 0;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a, n, your_money = 0, twins_money = 0;
+    vector <int> coins;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        coins.push_back(a);
+        twins_money += a;
     }
+    int i = coins.size() - 1;
+    sort(coins.begin(), coins.end());
+    while(your_money <= twins_money){
+        twins_money -= coins[i];
+        your_money += coins[i];
+        i--;
+    }
+    cout << coins.size() - i - 1 << endl;
+    return 0;
+}
