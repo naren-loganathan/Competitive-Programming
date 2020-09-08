33318ba32a71b05e157978e47d4d331ac7f2ca48
Naren Loganathan <naren.spirion@gmail.com>
2020-09-09 00:32:00 +0530

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/92297481

---
+++ codeforces/236/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int arr[26] = {};
+    int count = 0;
+    bool is_it_a_she = 0;
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++)
+        arr[s[i] - 97]++;
+    for(int i = 0; i < 26; i++)
+        if(arr[i]) count++;
+    if(count % 2 == 0)
+        is_it_a_she = 1;
+    if(is_it_a_she) 
+        cout << "CHAT WITH HER!" << endl;
+    else 
+        cout << "IGNORE HIM!" << endl;
+    return 0;
+}
