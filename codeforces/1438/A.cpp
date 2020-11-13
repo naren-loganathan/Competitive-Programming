6ce42058b29a0fce1f038c63a7b801f9c1f0316a
Naren Loganathan <naren.spirion@gmail.com>
2020-11-13 23:20:00 +0530

    Add solution for problem `A - Specific Tastes of Andre ` Link: https://codeforces.com/contest/1438/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1438/submission/98327229

---
+++ codeforces/1438/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+using namespace std;
+     
+typedef long long int ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define N 100005
+     
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << "1 ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
