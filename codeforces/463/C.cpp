72a60430449b96310ce6dabac9920371b9c4edeb
Naren Loganathan <naren.spirion@gmail.com>
2020-12-29 21:32:00 +0530

    Add solution for problem `C - Gargari and Bishops` Link: https://codeforces.com/contest/463/problem/C Tags: greedy, hashing, implementation, *1900 Ref: https://codeforces.com/contest/463/submission/102704981

--- codeforces/463/C.cpp
+++ codeforces/463/C.cpp
@@ -2,23 +2,52 @@
 #include <vector>
 #include <map>
 using namespace std;
-         
+     
 typedef long long ll;
 #define fi first
 #define se second
 #define mp make_pair
-         
+     
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, x1, y1, x2, y2; cin >> n;
     ll a[n][n];
-    map <int, ll> p, s;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> a[i][j];
-            p[i - j] += a[i][j];
-            s[i + j] += a[i][j];
+        }
+    }
+    map <int, ll> p, s;
+    for (int t = 0; t < 2; t++) {
+        for (int i = 0; i < n; i++) {
+            int j = 0, k = 0;
+            (t ? j = i : k = i);
+            int x = j, y = k;
+            ll sum = 0;
+            while (j < n && k < n) {
+                sum += a[j][k];
+                j++; k++;
+            }
+            p[x - y] = sum;
+        }
+    }
+    for (int t = 0; t < 2; t++) {
+        for (int i = 0; i < n; i++) {
+            int j, k;
+            if (t) {
+                j = 0; k = i;
+            }
+            else {
+                j = i; k = n - 1;
+            }
+            int x = j, y = k;
+            ll sum = 0;
+            while (j < n && k >= 0) {
+                sum += a[j][k];
+                j++; k--;
+            }
+            s[x + y] = sum;
         }
     }
     ll mw = -1, mb = -1;
