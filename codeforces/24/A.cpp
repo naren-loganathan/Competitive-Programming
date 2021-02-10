87142f712e7618bda70440771bd16ae5224e59c7
Naren Loganathan <naren.spirion@gmail.com>
2021-02-10 10:30:00 +0530

    Add solution for problem `A - Ring road` Link: https://codeforces.com/contest/24/problem/A Tags: graphs, *1400 Ref: https://codeforces.com/contest/24/submission/107027490

---
+++ codeforces/24/A.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+
+vector < vector < pair <int, int> > > adj(101);
+vector <bool> visited(101, 0);
+
+int money = 0;
+
+void dfs (int u) {
+    visited[u] = 1;
+    bool done = 1;
+    for (auto edge : adj[u]) {
+        if (!visited[edge.fi]) {
+            done = 0;
+            if (edge.se < 0) {
+                money -= edge.se;
+            }
+            dfs(edge.fi);
+        }
+    }
+    if (done) {
+        for (auto edge : adj[u]) {
+            if (edge.fi == 1) {
+                if (edge.se < 0) {
+                    money -= edge.se;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, sum_c = 0; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj[a].push_back(mp(b, c));
+        adj[b].push_back(mp(a, -c));
+        sum_c += c;
+    }
+    dfs(1);
+    cout << min(money, sum_c - money) << "\n";
+    return 0;
+}
