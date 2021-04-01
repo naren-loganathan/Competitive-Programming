1c0cbf594cb2f39d7f0ddbab701409fc8210971d
Naren Loganathan <naren.spirion@gmail.com>
2021-04-01 22:18:00 +0530

    Add solution for problem `F - Math` Link: https://codeforces.com/contest/1505/problem/F Tags: constructive algorithms, math Ref: https://codeforces.com/contest/1505/submission/111725369

---
+++ codeforces/1505/F.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a; cin >> a;
+    cout << 2 - a * a << '\n';
+    return 0;
+}
