dafe2ad5e472baaaaee6320e2616caa02199b5e0
Naren Loganathan <naren.spirion@gmail.com>
2021-04-22 10:24:00 +0530

    Add solution for problem `B - AGAGA XOOORRR` Link: https://codeforces.com/contest/1516/problem/B Tags: bitmasks, brute force, dp, greedy, *1500 Ref: https://codeforces.com/contest/1516/submission/113845548

--- codeforces/1516/B.cpp
+++ codeforces/1516/B.cpp
@@ -13,27 +13,50 @@ int main() {
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
-        vector <int> a(n), axor(n);
+        vector <int> a(n), bits[30];
         for (int i = 0; i < n; i++) {
             cin >> a[i];
-            axor[i] = a[i];
-            if (i) {
-                axor[i] ^= axor[i - 1];
+            for (int j = 0; j < 30; j++) {
+                bits[j].push_back((1LL << j) & a[i] ? 1 : 0);
             }
-        }
-        bool poss = 0;
-        for (int i = 0; i < n - 1 && !poss; i++) {
-            if (axor[i] == (axor[n - 1] ^ axor[i])) {
-                poss = 1; break;
-            } else {
-                for (int j = i + 1; j < n - 1; j++) {
-                    if (axor[i] == (axor[j] ^ axor[i]) && (axor[j] ^ axor[i]) == (axor[n - 1] ^ axor[j])) {
-                        poss = 1; break;
-                    }
+            if (i) {
+                for (int j = 0; j < 30; j++) {
+                    bits[j][i] += bits[j][i - 1];
                 }
             }
         }
-        cout << (poss ? "YES" : "NO") << '\n';
+        bool two = false, three = false;
+        for (int i = 0; i < n - 1; i++) {
+            bool ok = true;
+            for (int j = 0; j < 30; j++) {
+                if (bits[j][i] % 2 != (bits[j][n - 1] - bits[j][i]) % 2) {
+                    ok = false; break;
+                }
+            }
+            if (ok) {
+                two = true; break;
+            }
+        }
+        if (two) {
+            cout << "YES" << '\n';
+        } else if (n > 2) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n - 1; j++) {
+                    bool ok = true;
+                    for (int k = 0; k < 30; k++) {
+                        if ((bits[k][i] % 2) != ((bits[k][j] - bits[k][i]) % 2) || ((bits[k][j] - bits[k][i]) % 2) != ((bits[k][n - 1] - bits[k][j])) % 2) {
+                            ok = false; break;
+                        }
+                    }
+                    if (ok) {
+                        three = true; break;
+                    }
+                }
+            }
+            cout << (three ? "YES" : "NO") << '\n';
+        } else {
+            cout << "NO" << '\n';
+        }
     }
     return 0;
 }
